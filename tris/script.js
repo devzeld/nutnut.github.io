aae479a9f3396de555a00eb0537fbb8e10ad94b1
devzeld <zeld.exe.dev@gmail.com>
2025-02-03 23:25:37 +0100

    win added

--- tris/script.js
+++ tris/script.js
@@ -2,45 +2,61 @@
 const table = []; 
 let turn = 1;
 
-const winningPatterns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 6],
-    [0, 4, 8],
-    [2, 4, 6]
-]
-
 const grid = document.querySelectorAll("#grid #cell");
 
-grid.forEach(cell => {
-    cell.addEventListener("click", event => {
-        console.log(table.toString());
-        console.log(turn);
+for (let i = 0; i < grid.length; i++) {
+    
+    let cell = grid[i];
 
-        if (turn % 2 === 0) {
-            if (cell.querySelector("p").textContent === "" || cell.querySelector("p").textContent !== "O") {
-                table[turn - 1] = "X";
-                cell.querySelector("p").textContent = "X";
-            } else {
-                return;
-            }
-        } else{
-            if (cell.querySelector("p").textContent === "" || cell.querySelector("p").textContent !== "X") {
-                table[turn - 1] = "O";
-                cell.querySelector("p").textContent = "O";
-            }else{
-                return;
-            }
+    cell.addEventListener("click", event => {
+        console.table(table);
+        console.log(turn);
+        let sign = turn % 2 === 0 ? "X" : "O" 
+
+        if (table[i]) {
+            return;
         }
+
+        cell.querySelector("p").innerHTML = sign;
+        table[i] = sign;
 
         if(turn === 9){
             return;
         }
 
+        if (hasWon()) {
+            alert("Vittoria: " + sign);
+        }
+
         turn++;
-    })
-});
+    });
+}
+
+function hasWon() {
+    const winningPatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 6],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (let i = 0; i < winningPatterns.length; i++) {
+
+        const combination = winningPatterns[i];
+
+        const a = combination[0];
+        const b = combination[1];
+        const c = combination[2];
+        
+        if (table[a] && table[a] === table[b] && table[b] === table[c]) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
